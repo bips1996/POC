da482200cce46bad4bbe1a6e9f226c6515e6ebd3
Biplaba Samantaray <samantaraybiplaba@gmail.com>
2021-02-12 17:10:31 +0530

    CRUD Added

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Depends,BackgroundTasks,Request
 from pydantic import BaseModel 
 from typing import Optional
 import models
 from database import SessionLocal,engine
 from sqlalchemy.orm import Session
-from models import Employee , BUnits
+from models import Employee , BUnits 
+
+class createEmployee(BaseModel) :
+    id : int
+    name : str
+    email : str
+    position : str
+
+class bUnit(BaseModel) :
+    id = 0
+    title = ""
+    description =""
+    director = 0
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -16,5 +28,35 @@ def get_db():
     finally:
         db.close()
 
-# @app.post("/Employee")
-# def create_Employee(create_Employee : createEmployeeOne,)
+# @app.get("/")
+# def fetch_emp_data(id:int):
+
+@app.post("/Employees")
+def create_Employee(emp : createEmployee, db : Session = Depends(get_db)): 
+    employee = Employee()
+    employee.id = emp.id
+    employee.name = emp.name
+    employee.email = emp.email
+    employee.position = emp.position
+    db.add(employee)
+    db.commit()
+    # Background_tasks.add_task(fetch_emp_data,employee.id)
+    return{
+        "code":"Success",
+        "messege": "Created"
+    }
+
+@app.post("/BUnits")
+def create_BUnits(create_bunit: bUnit, db:Session = Depends(get_db)) :
+    bunit = BUnits()
+    bunit.id = create_bunit.id 
+    bunit.title = create_bunit.title
+    bunit.description = create_bunit.description
+    bunit.director = create_bunit.director
+    db.add(bunit)
+    db.commit()
+    return{
+        "code":"Success",
+        "messege": "New Bunit added"
+    }
+    
--- models.py
+++ models.py
@@ -5,18 +5,29 @@ from database import Base
 
 class Employee(Base):
     __tablename__ = "Employee"
-
     id = Column(Integer, primary_key=True, index=True)
-
-    name = Column(String)
+    name = Column(String,index = True)
     email = Column(String, unique=True, index=True)
     position = Column(String,index=True)
 
-
 class BUnits(Base):
     __tablename__ = "BUnits"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    Director = Column(Integer, ForeignKey("Employee.id"))
+    director = Column(Integer, ForeignKey("Employee.id")) 
+
+
+# {   
+#    "id" :  1,
+#     "title" :"IA",
+#     "description" :"Deals with development" ,
+#     "Director" : 1
+# }
+
+# {
+#     "id" :1,
+#     "name" : "Biplaba",
+#     "email" : "a@b.in",
+#     "position" : "intern"
+# }
